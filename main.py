dda1a2fbdb7a78fef690619db552bb1625a9c3c3
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-07-19 17:15:16 +0200

    updated params

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 # coding: utf-8
 
-import numpy as np
-from keras.utils.np_utils import to_categorical
+import os
 from argparse import ArgumentParser
 
+import numpy as np
+from keras.utils.np_utils import to_categorical
 
+from canister.modelbase import ModelBase
 from models.lstms import simple_lstm, bilstm
 from corpus import Indexer, Corpus
-
 from utils import root, take
-
-# Params
-BATCH_SIZE = 128
-NUM_BATCHES = 10000
 
 
 def build_set(corpus, idxr, size=2000):
@@ -27,8 +24,21 @@ def build_set(corpus, idxr, size=2000):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-r', '--root', type=str)
+    parser.add_argument('-d', '--db', type=str)
+    parser.add_argument('-e', '--epochs', type=int, default=10)
+    parser.add_argument('-b', '--batch_size', type=int, default=128)
+    parser.add_argument('-n', '--num_batches', type=int, default=10000)
+
     args = parser.parse_args()
     root = args.root if args.root else root
+    assert os.path.isdir(root)
+
+    BATCH_SIZE = args.batch_size
+    NUM_BATCHES = args.num_batches
+    EPOCHS = args.epochs
+
+    if args.db:
+        db = ModelBase(args.db)
 
     idxr = Indexer(reserved={0: 'padding', 1: 'OOV'})
     train = Corpus(root + 'train')
@@ -50,9 +60,7 @@ if __name__ == '__main__':
     model = bilstm(idxr.vocab_len(), metrics=['accuracy'])
 
     print("Starting training")
-    epochs = 5
-
-    for e in range(epochs):
+    for e in range(EPOCHS):
         losses = []
         batches = take(
             train.generate_batches(idxr, batch_size=BATCH_SIZE, oov_idx=1),
@@ -62,11 +70,11 @@ if __name__ == '__main__':
             y = to_categorical(y, nb_classes=idxr.vocab_len())
             loss, _ = model.train_on_batch(X, y)
             losses.append(loss)
-            if b % 50 == 0:
-                _, dev_acc = model.test_on_batch(X_dev, y_dev)
-                print("Epoch: %d, Loss: %.4f, Dev acc: %.4f" %
-                      (e, np.mean(losses), dev_acc))
-    # Final
+            if b % 150 == 0:
+                dev_loss, dev_acc = model.test_on_batch(X_dev, y_dev)
+                print("Epoch: %d, Loss: %.4f, Dev-loss: %.4f: Dev-acc: %.4f" %
+                      (e, np.mean(losses), dev_loss, dev_acc))
+
     _, test_acc = model.test_on_batch(X_test, y_test)
     print("Test acc: %.4f" % test_acc)
     model.save_weights('bilstm.h5')
