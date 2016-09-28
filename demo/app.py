3fbeb68e6fb0abedee14d6a1fee1486437eea594
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-09-28 13:16:43 +0200

    added REST method and better pred output

--- demo/app.py
+++ demo/app.py
@@ -1,12 +1,16 @@
 
-from bottle import get, post, run, request, template
+import json
+import decimal
+from bottle import get, post, run, request, response, template
 
 from ..predict import load_emb_bilstm, CharFiller
 from ..corpus import Indexer
 
+
 print("Loading model")
-idxr = Indexer.load('./fitted/emb_bilstm_indexer.json')  # path from project root
-m = load_emb_bilstm(idxr.vocab_len(), 28, lstm_layer=250, hidden_layer=150, rnn_layers=3)
+idxr = Indexer.load('./fitted/emb_bilstm_indexer.json')
+m = load_emb_bilstm(
+    idxr.vocab_len(), 28, lstm_layer=250, hidden_layer=150, rnn_layers=3)
 filler = CharFiller(m, idxr, 10)
 print("Done")
 
@@ -15,18 +19,53 @@ def render_template(text=None, output=None):
     return template('demo/landing', text=text, output=output)
 
 
+def get_prediction(pred, with_prob):
+    if with_prob:
+        return pred[0][0]
+    else:
+        return pred[0]
+
+
+def normalized_pred(text, pos, max_n, with_prob):
+    pred = filler.predict(text, pos, max_n=max_n, with_prob=with_prob)
+    if with_prob:
+        return [(c, float(prob)) for (c, prob) in pred]
+    else:
+        return pred
+
+
 @get('/')
 def landing():
     return render_template()
 
 
+@post('/fillrest')
+def fillrest():
+    response.headers['Content-Type'] = 'application/json'
+    try:
+        text = request.POST.text.strip()
+        max_n = int(request.POST.maxN) or 1
+        with_prob = request.POST.withProb or False
+        fillers = {pos: normalized_pred(text, pos, max_n, with_prob)
+                   for (pos, c) in enumerate(text) if c == '_'}
+        output = "".join([get_prediction(fillers[pos], with_prob)
+                          if pos in fillers else c
+                          for (pos, c) in enumerate(text)])
+        response.code = 200
+        return json.dumps({'output': output, 'fillers': fillers},
+                          cls=DecimalEncoder)
+    except Exception as e:
+        response.code = 500
+        return json.dumps({'error': str(e)})
+
+
 @post('/fill')
 def fill():
     text = request.POST.text.strip()
-    output = "".join([filler.predict(text, pos) if c == '_' else c
+    output = "".join([filler.predict(text, pos, with_prob=False)[0]
+                      if c == "_" else c
                       for (pos, c) in enumerate(text)])
     return render_template(text=text, output=output)
 
 if __name__ == '__main__':
     run(host='localhost', port=8083)
-
--- predict.py
+++ predict.py
@@ -13,25 +13,36 @@ def load_emb_bilstm(n_chars, emb_dim, **kwargs):
     return model
 
 
+def get_max_n(arr, max_n=1):
+    return arr.argsort()[-max_n:][::-1]
+
+
 class CharFiller(object):
-    def __init__(self, model, indexer, context):
+    def __init__(self, model, idxr, context):
         self.model = model
-        self.indexer = indexer
+        self.idxr = idxr
         self.context = context
 
-    def predict(self, s, pos):
-        left = self.indexer.transform(s[max(0, pos - self.context): pos])
-        right = self.indexer.transform(s[pos + 1: min(len(s), pos + self.context)])
-        left = pad(left, self.context, paditem=self.indexer.pad_code)
-        right = pad(right, self.context, paditem=self.indexer.pad_code, paddir='right')
+    def predict(self, s, pos, with_prob=False, max_n=1):
+        pad_code = self.idxr.pad_code
+        left = self.idxr.transform(s[max(0, pos - self.context): pos])
+        left = pad(left, self.context, paditem=self.idxr.pad_code)
+        right = self.idxr.transform(s[pos+1: min(len(s), pos+self.context)])
+        right = pad(right, self.context, paditem=pad_code, paddir='right')
         hist = np.asarray(left + right).reshape((1, self.context * 2))
-        return self.indexer.decode(np.argmax(self.model.predict(hist)))
+        pred = self.model.predict(hist)[0]  # model returns a embedded array
+        best = get_max_n(pred, max_n=max_n)
+        if with_prob:
+            return [(self.idxr.decode(code), pred[code]) for code in best]
+        else:
+            return [self.idxr.decode(code) for code in best]
 
 
 if __name__ == '__main__':
     # load charfiller
     idxr = Indexer.load('fitted/emb_bilstm_indexer.json')
-    m = load_emb_bilstm(idxr.vocab_len(), 28, lstm_layer=250, hidden_layer=150, rnn_layers=3)
+    m = load_emb_bilstm(idxr.vocab_len(), 28,
+                        lstm_layer=250, hidden_layer=150, rnn_layers=3)
     filler = CharFiller(m, idxr, 10)
-
-    
+    filler.predict("this is a sentence to be filled with a lot of characters",
+                   15, max_n=5, with_prob=True)
