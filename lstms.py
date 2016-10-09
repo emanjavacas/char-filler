2d1c1d217f58bf60b6630feba0382966a1f92e5a
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-10-09 14:12:18 +0200

    refs & bug fixes

--- lstms.py
+++ lstms.py
@@ -15,39 +15,44 @@ def simple_lstm(n_chars, context=10, hidden_layer=128):
     return model
 
 
-def bilstm_layer(input_layer, lstm_layer, rnn_layers, dropout):
+def bilstm_layer(input_layer, lstm_dims, rnn_layers, dropout):
     lstm = None
+    if isinstance(lstm_dims, (list, tuple)):
+        lstm_dims = lstm_dims
+    else:
+        assert isinstance(lstm_dims, int)
+        lstm_dims = [lstm_dims] * rnn_layers
     for i in range(rnn_layers):
         if i == 0:
             nested = input_layer
         else:
             nested = lstm
         wrapped = LSTM(
-            output_dim=lstm_layer, activation='tanh', return_sequences=True,
+            output_dim=lstm_dims[i], activation='tanh', return_sequences=True,
             dropout_W=dropout, dropout_U=dropout, name='bistm_%d' % i)
         lstm = Bidirectional(wrapped, merge_mode='sum')(nested)
     return lstm
 
 
-def bilstm(n_chars, context=10, lstm_layer=128, hidden_layer=250, rnn_layers=1,
+def bilstm(n_chars, context=10, lstm_dims=128, hidden_dim=250, rnn_layers=1,
            dropout=0.0):
     in_layer = Input(shape=(context * 2, n_chars), name='input')
-    lstm = bilstm_layer(in_layer, lstm_layer, rnn_layers, dropout=dropout)
-    dense = TimeDistributed(hidden_layer, name='dense')(lstm)
+    lstm = bilstm_layer(in_layer, lstm_dims, rnn_layers, dropout=dropout)
+    dense = TimeDistributed(Dense(hidden_dim), name='dense')(lstm)
     flattened = Flatten(name='flattened')(dense)
     out_layer = Dense(n_chars, activation='softmax')(flattened)
     model = Model(input=in_layer, output=out_layer)
     return model
 
 
-def emb_bilstm(n_chars, emb_dim, context=10, lstm_layer=128, hidden_layer=250,
+def emb_bilstm(n_chars, emb_dim, context=10, lstm_dims=128, hidden_dim=250,
                rnn_layers=1, dropout=0.0):
     in_layer = Input(shape=(context * 2,), dtype='int32', name='input')
     emb_layer = Embedding(
         input_dim=n_chars, output_dim=emb_dim, input_dtype='int32',
         name='emb')(in_layer)
-    lstm = bilstm_layer(emb_layer, lstm_layer, rnn_layers, dropout=dropout)
-    dense = TimeDistributed(Dense(hidden_layer), name='dense')(lstm)
+    lstm = bilstm_layer(emb_layer, lstm_dims, rnn_layers, dropout=dropout)
+    dense = TimeDistributed(Dense(hidden_dim), name='dense')(lstm)
     flattened = Flatten(name='flattened')(dense)
     out_layer = Dense(n_chars, activation='softmax', name='output')(flattened)
     model = Model(input=in_layer, output=out_layer)
