6e44d67acc7e079270ea26959a6b11fbfea9313b
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-11-10 13:48:37 +0100

    minor changes

--- bilstm.py
+++ bilstm.py
@@ -10,6 +10,21 @@ from lasagne.nonlinearities import softmax, tanh
 import lasagne
 
 
+def bilstm_layer(input_left, input_right, lstm_dim, n_layers=1):
+    for n in range(n_layers):
+        left_lstm = LSTMLayer(
+            input_left, lstm_dim, only_return_final=True, backwards=True)
+        right_lstm = LSTMLayer(
+            input_right, lstm_dim, only_return_final=True, backwards=False)
+        dense_left = DenseLayer(
+            left_lstm, num_units=lstm_dim, nonlinearity=tanh)
+        dense_right = DenseLayer(
+            right_lstm, num_units=lstm_dim, nonlinearity=tanh)
+        input_left = dense_left
+        input_right = dense_right
+    return ElemwiseSumLayer([dense_left, dense_right])
+
+
 class BILSTM(object):
     def __init__(self, emb_dim, lstm_dim, vocab_size):
         self.emb_dim = emb_dim
@@ -24,15 +39,7 @@ class BILSTM(object):
             left_in, input_size=vocab_size, output_size=emb_dim, W=self.emb_W)
         emb_right = EmbeddingLayer(
             left_in, input_size=vocab_size, output_size=emb_dim, W=self.emb_W)
-        left_lstm = LSTMLayer(
-            emb_left, lstm_dim, only_return_final=True)
-        right_lstm = LSTMLayer(
-            emb_right, lstm_dim, only_return_final=True, backwards=True)
-        dense_left = DenseLayer(
-            left_lstm, num_units=lstm_dim, nonlinearity=tanh)
-        dense_right = DenseLayer(
-            right_lstm, num_units=lstm_dim, nonlinearity=tanh)
-        merged = ElemwiseSumLayer([dense_left, dense_right])
+        merged = bilstm_layer(emb_left, emb_right, lstm_dim)
         output = DenseLayer(merged, num_units=vocab_size, nonlinearity=softmax)
 
         # T.nnet.categorical_crossentropy allows to represent true distribution
@@ -68,8 +75,23 @@ class BILSTM(object):
         else:
             return self._train(batch_left, batch_right, batch_y)
 
-    def fit(self):
-        pass
+    def fit(self, batch_gen, epochs, batch_size, batches=1, **kwargs):
+        """
+        Parameters:
+        -----------
+        batch_gen: generator function returning batch tuples ((left, right), y)
+        epochs: int, number of epochs
+        batch_size: int, input to batch_gen
+        batches: int, how many batches in between loss report
+        """
+        for e in epochs:
+            losses = []
+            for b, ((left, right), y) in enumerate(batch_gen(batch_size)):
+                losses.append(self.train_on_batch(left, right, y, **kwargs))
+                if b % batches == 0:
+                    print("Epoch [%d], batch [%d], Avg. training loss [%f]" %
+                          (e, b, np.mean(losses)))
+            yield losses
 
     def predict(self, left_in, right_in, max_n=False, return_probs=False):
         """
@@ -94,3 +116,8 @@ class BILSTM(object):
 
     def load(self):
         pass
+
+
+
+if __name__ == '__main__':
+    pass
